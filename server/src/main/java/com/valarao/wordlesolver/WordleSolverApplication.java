2046694d14d0e53df59efeeae0978df22c3d7d51
valarao <miguel.valarao@gmail.com>
2022-02-09 13:10:14 -0800

    Setup Spring Boot boilerplate for server backend

---
+++ server/src/main/java/com/valarao/wordlesolver/WordleSolverApplication.java
@@ -0,0 +1,13 @@
+package com.valarao.wordlesolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordleSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordleSolverApplication.class, args);
+	}
+
+}
