96ac2f6d20e4c1c8550d87c41d5f3c3638fac211
valarao <miguel.valarao@gmail.com>
2022-02-11 15:01:08 -0800

    Add javadocs

--- src/main/java/com/valarao/wordlesolver/calculator/ScoreCalculator.java
+++ src/main/java/com/valarao/wordlesolver/calculator/ScoreCalculator.java
@@ -5,14 +5,14 @@ import com.valarao.wordlesolver.model.PastGuess;
 import java.util.List;
 
 /**
- * Class to perform score calculations based on a list of past guesses.
+ * Class to perform score calculations.
  */
 public interface ScoreCalculator<T> {
 
     /***
      *
+     * @param allWords List of all words considered to guess.
      * @param pastGuesses List of past Wordle guesses given by an application user.
-     * @
      * @return Calculated scores.
      */
     List<T> calculate(List<String> allWords, List<PastGuess> pastGuesses);
