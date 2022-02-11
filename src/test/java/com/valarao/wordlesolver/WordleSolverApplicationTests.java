a4c009905809989e1882b7598e4892f4267afc6c
Miguel Valarao <miguel.valarao@gmail.com>
2022-02-10 18:44:59 -0800

    Setup Java Spring project structure (#2)
    * Setup Spring Boot boilerplate for server backend
    
    * Move into main folder
    
    * Add MIT License

---
+++ src/test/java/com/valarao/wordlesolver/WordleSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valarao.wordlesolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
