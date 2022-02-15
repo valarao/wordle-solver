e7c17c897347dc888371793ead410fba7009a772
Miguel Valarao <miguel.valarao@gmail.com>
2022-02-15 13:01:35 -0800

    Setup frontend skeleton (#37)
    * Setup frontend skeleton
    
    * Disable long test

---
+++ src/main/ui/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Wordle Solver
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
