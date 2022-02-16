fac9a3dbd5e8b10c493d976500e429bcd48ea7c4
Miguel Valarao <miguel.valarao@gmail.com>
2022-02-15 20:37:32 -0800

    Register clicks on keyboard (#40)

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -4,21 +4,33 @@ import Grid from './components/Grid/Grid';
 import Header from './components/Header';
 import Keyboard from './components/Keyboard/Keyboard';
 import Recommendation from './components/Recommendation/Recommendation';
+import { NUMBER_OF_ATTEMPTS, WORD_LENGTH } from './util/constants';
 
 function App() {
   const [guessIndex, setGuessIndex] = useState(0);
+  const [previousGuesses, setPreviousGuesses] = useState([]);
   const [userGuess, setUserGuess] = useState("");
   const handleUserKeyPress = useCallback(event => {
     const { key, keyCode } = event;
+
     setUserGuess(prevUserGuess => {
       if (keyCode >= 65 && keyCode <= 90 && prevUserGuess.length < 5) {
         return `${prevUserGuess}${key.toUpperCase()}`
       } else if (keyCode === 8 && prevUserGuess.length > 0) {
         return prevUserGuess.substring(0, prevUserGuess.length - 1);
+      } else if (keyCode === 13 && userGuess.length === WORD_LENGTH) {
+        if (guessIndex < NUMBER_OF_ATTEMPTS - 1) {
+          setGuessIndex(guessIndex + 1);
+          setPreviousGuesses([...previousGuesses, userGuess]);
+      } else {
+          setGuessIndex(0);
+          setPreviousGuesses([]);
+      }
+        return '';
       } else {
         return prevUserGuess;
       }});
-  }, []);
+  }, [guessIndex, previousGuesses, userGuess]);
 
   useEffect(() => {
     window.addEventListener('keydown', handleUserKeyPress);
@@ -35,8 +47,16 @@ function App() {
         <Grid
           userGuess={userGuess}
           guessIndex={guessIndex}
+          previousGuesses={previousGuesses}
         />
-        <Keyboard setGuessIndex={setGuessIndex} />
+        <Keyboard
+          userGuess={userGuess}
+          setUserGuess={setUserGuess}
+          guessIndex={guessIndex}
+          setGuessIndex={setGuessIndex}
+          previousGuesses={previousGuesses}
+          setPreviousGuesses={setPreviousGuesses}
+        />
       </div>
     </div>
   );
--- src/main/ui/src/components/Keyboard/KeyboardRow.js
+++ src/main/ui/src/components/Keyboard/KeyboardRow.js
@@ -2,14 +2,26 @@ import DeleteKey from "./DeleteKey";
 import Key from "./Key";
 import SolveKey from "./SolveKey";
 
-const KeyboardRow = ({rowLetters, isLastRow}) => {
+const KeyboardRow = ({ rowLetters, isLastRow, guessIndex, setGuessIndex, previousGuesses, setPreviousGuesses, userGuess, setUserGuess }) => {
     return (
         <div className='KeyboardRow'>
-            {isLastRow && <SolveKey />}
-            {rowLetters.map(letter => <Key letter={letter} key={letter}/>)}
-            {isLastRow && <DeleteKey />}
+            {isLastRow &&
+                <SolveKey
+                    guessIndex={guessIndex}
+                    setGuessIndex={setGuessIndex}
+                    previousGuesses={previousGuesses}
+                    setPreviousGuesses={setPreviousGuesses}
+                    userGuess={userGuess}
+                    setUserGuess={setUserGuess}
+                />}
+            {rowLetters.map(letter => <Key letter={letter} key={letter} userGuess={userGuess} setUserGuess={setUserGuess} />)}
+            {isLastRow &&
+                <DeleteKey
+                    userGuess={userGuess}
+                    setUserGuess={setUserGuess}
+                />}
         </div>
     );
 }
- 
+
 export default KeyboardRow;
