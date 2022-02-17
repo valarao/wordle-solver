fa2f48bf799b3acc4befb1fd441cd864dc104384
valarao <miguel.valarao@gmail.com>
2022-02-17 02:50:36 -0800

    Add comment

--- src/main/java/com/valarao/wordlesolver/AppController.java
+++ src/main/java/com/valarao/wordlesolver/AppController.java
@@ -3,6 +3,9 @@ package com.valarao.wordlesolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller to handle mapping point to link frontend client application.
+ */
 @Controller
 public class AppController {
 
