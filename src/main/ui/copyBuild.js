b6758d9691199d551fc73f007acf174487d44a9e
valarao <miguel.valarao@gmail.com>
2022-02-17 02:59:54 -0800

    Restructure copy build file

--- src/main/ui/copyBuild.js
+++ src/main/ui/copyBuild.js
@@ -1,25 +1,29 @@
 const fs = require('fs');
 
-const SOURCE_FOLDER = `${__dirname}/build`;
-const TARGET_FOLDER = `${__dirname}/../resources/static`;
-const SUBFOLDERS = ['', '/static', '/static/css', '/static/js'];
-
 /**
  * Script to copy frontend build files to resources folder to be packaged with server application.
  */
-SUBFOLDERS.forEach((subfolder) => {
-    const sourceFolderPath = `${SOURCE_FOLDER}/${subfolder}`;
-    const targetFolderPath = `${TARGET_FOLDER}/${subfolder}`;
+const copyBuild = () => {
+    const SOURCE_FOLDER = `${__dirname}/build`;
+    const TARGET_FOLDER = `${__dirname}/../resources/static`;
+    const SUBFOLDERS = ['', '/static', '/static/css', '/static/js'];
+    
+    SUBFOLDERS.forEach((subfolder) => {
+        const sourceFolderPath = `${SOURCE_FOLDER}/${subfolder}`;
+        const targetFolderPath = `${TARGET_FOLDER}/${subfolder}`;
+    
+        if (!fs.existsSync(targetFolderPath)){
+            fs.mkdirSync(targetFolderPath);
+        }
+    
+        const files = fs.readdirSync(sourceFolderPath);
+        files.forEach((file) => {
+            fs.copyFile(
+                `${sourceFolderPath}/${file}`,
+                `${targetFolderPath}/${file}`,
+                (err) => {  });
+        });
+    });    
+}
 
-    if (!fs.existsSync(targetFolderPath)){
-        fs.mkdirSync(targetFolderPath);
-    }
-
-    const files = fs.readdirSync(sourceFolderPath);
-    files.forEach((file) => {
-        fs.copyFile(
-            `${sourceFolderPath}/${file}`,
-            `${targetFolderPath}/${file}`,
-            (err) => {  });
-    });
-});
+copyBuild();
