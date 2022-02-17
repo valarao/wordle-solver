8a4f854cdbf23dc6cbc5816ab5452d23956e1f05
valarao <miguel.valarao@gmail.com>
2022-02-16 23:29:58 -0800

    Delete redundant import

--- src/main/java/com/valarao/wordlesolver/controller/ScoreController.java
+++ src/main/java/com/valarao/wordlesolver/controller/ScoreController.java
@@ -15,8 +15,6 @@ import com.valarao.wordlesolver.validation.GuessValidator;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
