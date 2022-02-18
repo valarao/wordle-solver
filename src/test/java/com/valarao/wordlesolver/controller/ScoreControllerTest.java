dfc19d561124063386428bb28151a0795e6be171
valarao <miguel.valarao@gmail.com>
2022-02-18 00:29:14 -0800

    Remove

--- src/test/java/com/valarao/wordlesolver/controller/ScoreControllerTest.java
+++ src/test/java/com/valarao/wordlesolver/controller/ScoreControllerTest.java
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
